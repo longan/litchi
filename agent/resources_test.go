d055743a71b2fff55a8506ab5ea00c861e61a813
tobe <tobeg3oogle@gmail.com>
2015-04-07 21:52:09 +0800

    Fix log in resources test

--- agent/resources_test.go
+++ agent/resources_test.go
@@ -8,6 +8,6 @@ func TestConstructResource(t *testing.T) {
 	r := ConstructResources(1, 1, 1, 1, 1, 1)
 
 	if r.CpuTotal != 1 || r.CpuUsage != 1 || r.MemoryTotal != 1 || r.MemoryUsage != 1 || r.DiskTotal != 1 || r.DiskUsage != 1 {
-		t.Fatal("disk stat read fail")
+		t.Fatal("Fail to construct resources struct")
 	}
 }
